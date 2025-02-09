1defd00f89a8ce4709827b32de706ad133186fa8
DemonVenom666 <116897771+DemonVenom666@users.noreply.github.com>
2025-02-09 14:42:05 +0100

    testing

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 console.log("gagagag");
+
 function test() {
-    console.log("test1");
+    console.log("Функція test викликана!");
 }
+
+test(); // Викличе функцію без помилки
+
+
+let test3 = "Hello";
+console.log(test3); // Виведе: Hello
