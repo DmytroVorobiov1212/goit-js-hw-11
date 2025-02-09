7d0c760adfea5b0840a04bd62fa1f90e4921170c
DmytroVorobiov1212 <116897771+DmytroVorobiov1212@users.noreply.github.com>
2025-02-09 14:09:52 +0000

    Deploying to gh-pages from @ DmytroVorobiov1212/goit-js-hw-11@2e33dc436ab07eb65f08ef87811f14f7bdaa2eef 🚀

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
-(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const o of t.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&s(o)}).observe(document,{childList:!0,subtree:!0});function n(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function s(e){if(e.ep)return;e.ep=!0;const t=n(e);fetch(e.href,t)}})();function i(){console.log("Функція test викликана!")}i();let c="Hello";console.log(c);setInterval(i,2e3);
+import{a as f,i as c,S as m}from"./assets/vendor-SnYWMg9o.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))l(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const a of r.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&l(a)}).observe(document,{childList:!0,subtree:!0});function i(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function l(e){if(e.ep)return;e.ep=!0;const r=i(e);fetch(e.href,r)}})();const h="45785559-b0577f06fb94f4ced9a4d3280",g="https://pixabay.com/api/";function b(s){try{return s.includes(" ")&&s.replace(/\s+/g,"+"),f.get(`${g}`,{params:{key:h,q:s,image_type:"photo",orientation:"horizontal",safesearch:!0}})}catch(t){c.error({title:"Error",message:"Sorry! The site is currently unavailable. Please try later!"}),console.error(t.message)}}function v(s){return s.map(({webformatURL:t,largeImageURL:i,tags:l,likes:e,views:r,comments:a,downloads:y})=>`<li class="gallery-item">
+      <a class="gallery-link" href="${i}">
+        <img class="gallery-image" src="${t}" alt="${l}">
+      </a>
+      <div class="thumb-block">
+        <div class="block">
+          <h2 class="title">Likes</h2>
+          <p class="amount">${e}</p>
+        </div>
+        <div class="block">
+          <h2 class="title">Views</h2>
+          <p class="amount">${r}</p>
+        </div>
+        <div class="block">
+          <h2 class="title">Comments</h2>
+          <p class="amount">${a}</p>
+        </div>
+        <div class="block">
+          <h2 class="title">Downloads</h2>
+          <p class="amount">${y}</p>
+        </div>
+      </div>
+    </li>`).join("")}const u=document.querySelector(".js-form"),d=document.querySelector(".js-gallery"),o=document.querySelector(".loader");let n="",p;o.style.display="none";u.addEventListener("submit",L);function L(s){if(s.preventDefault(),d.innerHTML="",o.style.display="block",n=s.target.elements.query.value.trim(),n==="")return c.info({title:"Hello",message:"Please enter search text!"}),o.style.display="none",u.reset();b(n).then(t=>{if(d.insertAdjacentHTML("beforeend",v(t.data.hits)),!t.data.hits.length){c.error({title:"Error",message:"Sorry, there are no images matching your search query. Please try again!"}),o.style.display="none";return}o.style.display="none",p=new m(".gallery a",{captions:!0,captionsData:"alt",captionDelay:250}),p.refresh()}).catch(t=>{o.style.display="none",console.log(`${t}`)}),u.reset()}
 //# sourceMappingURL=index.js.map
